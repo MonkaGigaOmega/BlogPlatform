12e032e569f65fd77b2d31e92a9fc5c4ead065d7
Gosha Shestakov <gosha3103@mail.ru>
2025-05-08 14:04:02 +0300

    маркдаун заработал

--- src/components/ArticlePage/ArticlePage.js
+++ src/components/ArticlePage/ArticlePage.js
@@ -2,6 +2,8 @@ import React, { useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import { useSelector, useDispatch } from 'react-redux'
 import { fetchArticleBySlug } from '../../redux/actions/articleActions'
+import MarkdownIt from 'markdown-it'
+import './ArticlePage.css'
 
 const ArticlePage = () => {
   const dispatch = useDispatch()
@@ -12,14 +14,32 @@ const ArticlePage = () => {
   }, [slug, dispatch])
 
   const article = useSelector((state) => state.articles.currentArticle)
+  const loading = useSelector((state) => state.articles.loadingArticle)
+  const error = useSelector((state) => state.articles.error)
 
-  if (!article) {
-    return <p>Загружается статья...</p>
-  }
+  if (loading) return <p>Загружается статья...</p>
+  if (error) return <p>Произошла ошибка: {error}</p>
+  if (!article) return <p>Статья не найдена.</p>
+
+  const md = new MarkdownIt({ html: true, linkify: true, typographer: true })
+
+  const fixMarkdown = (mdText) =>
+    mdText
+      .replace(/^(#{1,6})([^\s#])/gm, '$1 $2') // пробел после #
+      .replace(/^(-|\+|\*)([^\s])/gm, '$1 $2') // пробел после маркера списка
+      .replace(/^(\d+\.)[^\s]/gm, '$1 ') // пробел после цифры в списке
+
+  const rawBody = article.body || ''
+  const fixedBody = fixMarkdown(rawBody)
+  const articleContent = md.render(fixedBody)
+
   return (
     <div>
       <h1>{article.title}</h1>
-      <p>{article.content}</p>
+      <div
+        className="article-content"
+        dangerouslySetInnerHTML={{ __html: articleContent }}
+      />
     </div>
   )
 }
--- src/redux/reducers/articleReducer.js
+++ src/redux/reducers/articleReducer.js
@@ -1,37 +1,45 @@
 const initialState = {
-  articles: [],
-  loading: false,
+  articles: [], // для списка статей
+  loadingArticles: false, // состояние загрузки списка статей
+  loadingArticle: false, // состояние загрузки конкретной статьи
   error: null,
   articlesCount: 0,
-  currentArticle: null,
+  currentArticle: null, // конкретная статья
 }
 
 export default function articleReducer(state = initialState, action) {
   switch (action.type) {
     case 'FETCH_ARTICLES_START':
-      return { ...state, loading: true, error: null }
-    case 'FETCH_ARTICLES_SUCCESS': {
+      return { ...state, loadingArticles: true, error: null }
+
+    case 'FETCH_ARTICLES_SUCCESS':
       return {
         ...state,
-        loading: false,
+        loadingArticles: false,
         articles: action.payload.articles,
         articlesCount: action.payload.articlesCount,
       }
-    }
+
     case 'FETCH_ARTICLES_FAILURE':
-      return { ...state, loading: false, error: action.payload }
+      return { ...state, loadingArticles: false, error: action.payload }
+
     case 'FETCH_ARTICLE_START':
-      return { ...state, loading: true, error: null, currentArticle: null }
+      return {
+        ...state,
+        loadingArticle: true,
+        error: null,
+        currentArticle: null,
+      }
 
     case 'FETCH_ARTICLE_SUCCESS':
       return {
         ...state,
-        loading: false,
+        loadingArticle: false,
         currentArticle: action.payload.article,
       }
 
     case 'FETCH_ARTICLE_FAILURE':
-      return { ...state, loading: false, error: action.payload }
+      return { ...state, loadingArticle: false, error: action.payload }
 
     default:
       return state
